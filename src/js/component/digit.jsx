a6b3c5541fd673911aa8cd1aec9fd6f4a16bd1f5
Ines-sanz <inessanzm@hotmail.es>
2024-10-23 19:33:39 +0000

    feat:first commit

---
+++ src/js/component/digit.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+
+export const Digit = ({ value }) => (
+  <div class="bg- text-white display-1 fw-bold border border-2 rounded p-4 mx-4 bg-secondary bg-gradient">
+    {value}
+  </div>
+);
